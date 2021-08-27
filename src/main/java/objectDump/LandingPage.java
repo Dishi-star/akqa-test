34fef4d96f16609714fd37554d0792f906d56f76
Dishi-star <56906821+Dishi-star@users.noreply.github.com>
2021-08-27 18:44:52 +1000

    Update LandingPage.java

--- src/main/java/objectDump/LandingPage.java
+++ src/main/java/objectDump/LandingPage.java
@@ -21,10 +21,11 @@ public class LandingPage {
 	private By serchIcon = By.cssSelector("button[id='crossIcon']");
 	private By store = By.xpath("//div[@id='panel1bh-header']");
 	private By collect = By.xpath("//div[@data-locator='Store & AvailabilityResult-Click & Collect']");
-
 	private By cartButton = By.cssSelector("a[href='/dulux-ceiling-white-paint_p1370213\'] button[type='button']");
 	private By cartlink = By.xpath("//a[@id='icon-cart']");
 	private By productName = By.xpath("//div[@class='ProductName']/a/h4");
+	private By productPrice=By.cssSelector("div[data-locator='tile_TotalPrice']");
+	private By productQty=By.cssSelector("div[data-locator='title_Total']");
 
 	public WebElement search() {
 		return driver.findElement(searchitem);
